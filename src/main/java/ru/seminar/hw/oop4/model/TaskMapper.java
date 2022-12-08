46e88af008fcfafcb45f41ee89da4c47c64d786c
Pavel Komlevskiy <hekpacoff@gmail.com>
2022-12-08 11:58:12 +0300

    разделение по ;

--- src/main/java/ru/seminar/hw/oop4/model/TaskMapper.java
+++ src/main/java/ru/seminar/hw/oop4/model/TaskMapper.java
@@ -2,12 +2,11 @@ package ru.seminar.hw.oop4.model;
 
 public class TaskMapper {
     public String map(Task task) {
-        return String.format("%s,%s,%s,%s,%s,%s,%s,%s", task.getId(), task.getTitle(), task.getDescription() ,task.getFullname(), task.getDate(), task.getTime(), task.getDeadline(), task.getLevel());
+        return String.format("%s;%s;%s;%s;%s;%s;%s;%s", task.getId(), task.getTitle(), task.getDescription() ,task.getFullname(), task.getDate(), task.getTime(), task.getDeadline(), task.getLevel());
     }
 
     public Task map(String line) {
-        String[] lines = line.split(",");
-        System.out.println(lines[5]);
+        String[] lines = line.split(";");
         return new Task(lines[0], lines[1], lines[2], lines[3], lines[4], lines[5], lines[6], lines[7]);
     }
 }
